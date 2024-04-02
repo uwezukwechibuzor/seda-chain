49aed138c694dd644fecaa9b17add2054ff7893b
uwezukwechibuzor <uwezukwechibuzor@gmail.com>
2024-04-02 14:50:07 +0100

    Update error code

--- x/wasm-storage/types/errors.go
+++ x/wasm-storage/types/errors.go
@@ -4,12 +4,12 @@ import errorsmod "cosmossdk.io/errors"
 
 // x/wasm-storage module sentinel errors
 var (
-	ErrDataRequestWasmExists   = errorsmod.Register(ModuleName, 1, "data Request Wasm with given hash already exists")
-	ErrOverlayWasmExists       = errorsmod.Register(ModuleName, 2, "overlay Wasm with given hash already exists")
-	ErrInvalidAuthorityAddress = errorsmod.Register(ModuleName, 3, "invalid authority address")
-	ErrInvalidAuthority        = errorsmod.Register(ModuleName, 4, "invalid authority")
-	ErrInvalidSenderAddress    = errorsmod.Register(ModuleName, 5, "invalid sender address")
-	ErrInvalidAdminAddress     = errorsmod.Register(ModuleName, 6, "invalid admin address")
-	ErrWasmNotGzipCompressed   = errorsmod.Register(ModuleName, 7, "wasm is not gzip compressed")
-	ErrInstantiateContract     = errorsmod.Register(ModuleName, 8, "error during contract instantiation")
+	ErrDataRequestWasmExists   = errorsmod.Register(ModuleName, 2, "data Request Wasm with given hash already exists")
+	ErrOverlayWasmExists       = errorsmod.Register(ModuleName, 3, "overlay Wasm with given hash already exists")
+	ErrInvalidAuthorityAddress = errorsmod.Register(ModuleName, 4, "invalid authority address")
+	ErrInvalidAuthority        = errorsmod.Register(ModuleName, 5, "invalid authority")
+	ErrInvalidSenderAddress    = errorsmod.Register(ModuleName, 6, "invalid sender address")
+	ErrInvalidAdminAddress     = errorsmod.Register(ModuleName, 7, "invalid admin address")
+	ErrWasmNotGzipCompressed   = errorsmod.Register(ModuleName, 8, "wasm is not gzip compressed")
+	ErrInstantiateContract     = errorsmod.Register(ModuleName, 9, "error during contract instantiation")
 )
